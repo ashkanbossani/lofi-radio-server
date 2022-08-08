8e30ad4605841ad73b2a2ac058d3dc7229914879
ashkanbossani <abossani@hotmail.com>
2022-08-07 17:44:07 -0700

    fixed problem is post request

--- index.js
+++ index.js
@@ -13,52 +13,50 @@ app.use(express.urlencoded({ extended: true }))
 
 const PORT = 5050
 
-
-app.post("/login", async (req, res) => {
-  const { code } = req.body
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+app.post("/login", (req, res) => {
+    const { code } = req.body
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    })
+  
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(({ body: { access_token, refresh_token, expires_in } }) => {
+        res.json({ access_token, refresh_token, expires_in })
+      })
+      .catch(console.error)
   })
 
-  try {
-    const {
-      body: { access_token, refresh_token, expires_in },
-    } = await spotifyApi.authorizationCodeGrant(code)
-
-    res.json({ access_token, refresh_token, expires_in })
-  } catch (err) {
-    console.log(err)
-    res.sendStatus(400)
-  }
-})
-
-app.post("/refresh", async (req, res) => {
-  const { refreshToken } = req.body
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
+app.post("/refresh", (req, res) => {
+    const { refreshToken } = req.body
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken,
+    })
+  
+    spotifyApi
+      .refreshAccessToken()
+      .then(({ body: { access_token, expires_in } }) => {
+        res.json({ access_token, expires_in })
+      })
+      .catch((err) => console.log(err))
   })
 
-  try {
-    const {
-      body: { access_token, expires_in },
-    } = await spotifyApi.refreshAccessToken()
-    res.json({ access_token, expires_in })
-  } catch (err) {
-    console.log(err)
-    res.sendStatus(400)
-  }
-})
 
-app.get("/lyrics", async (req, res) => {
-  const { artist, track } = req.query
-  const lyrics = (await lyricsFinder(artist, track)) || "No Lyrics Found"
-  res.json({ lyrics })
-})
+app.get("/lyrics", (req, res, next) => {
+    const { artist, track } = req.query
+    lyricsFinder(artist, track)
+      .then(lyrics => {
+        res.json({ lyrics })
+      })
+      .catch(err => {
+        next(err)
+      })
+  })
 
 app.listen(PORT, err => {
   if (err) console.log(err)
